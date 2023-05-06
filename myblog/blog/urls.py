58d3dc5e34618c9df6b3d09fa5af50c3333d8851
random name <testmai@gmail.com>
2023-05-06 22:25:27 +0200

    more views, more urls and some templates

--- myblog/blog/urls.py
+++ myblog/blog/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from blog import views
 
 urlpatterns = [
+    #     url       view                        html file
     path('', views.PostListView.as_view(), name='post_list'),
     path('about/', views.AboutView.as_view(), name='about'),
     # path('post/(?P<pk>\d+)/$', views.PostDetailView.as_view(), name='post_detail'),
@@ -10,5 +11,9 @@ urlpatterns = [
     path('post/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post_edit'),
     path('post/<int:pk>/remove/', views.PostDeleteView.as_view(), name='post_remove'),
     path('drafts', views.DraftListView.as_view(), name='post_draft_list'),
+    path('post/<int:pk>/comment/', views.add_comment_to_post., name='add_comment_to_post'),
+    path('comment/<int:pk>/approve/', views.comment_approve, name='comment_approve'),
+    path('comment/<int:pk>/delete/', views.comment_remove, name='comment_remove'),
+    path('post/<int:pk>/publish/', views.post_publish, name='post_publish'),
 
 ]
